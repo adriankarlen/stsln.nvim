a7342ebfe7c5a969806706da6f040e003d3d5db3
Karlén <adrian.karlen@spp.se>
2024-04-23 21:55:02 +0200

    feat: add harpoon items

--- lua/stsln.lua
+++ lua/stsln.lua
@@ -1,16 +1,5 @@
 local M = {}
-local utilities = require("stsln.utilities")
-
-local set_stsln = function()
-  for _, win in pairs(vim.api.nvim_list_wins()) do
-    if vim.api.nvim_get_current_win() == win then
-      vim.wo[win].statusline = "%!v:lua.require\'stsln\'.load(\"active\")"
-    elseif vim.api.nvim_buf_get_name(0) ~= "" then
-      vim.wo[win].statusline = "%!v:lua.require\'stsln\'.load()"
-    end
-  end
-end
-
+local utilities = require "stsln.utilities"
 
 local mode_colors = {
   ["n"] = "#9ccfd8",
@@ -61,21 +50,55 @@ local mode_icons = {
   ["R"] = " ",
 }
 
+local set_stsln = function()
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_get_current_win() == win then
+      vim.wo[win].statusline = "%!v:lua.require'stsln'.load(\"active\")"
+    elseif vim.api.nvim_buf_get_name(0) ~= "" then
+      vim.wo[win].statusline = "%!v:lua.require'stsln'.load()"
+    end
+  end
+end
+
+local get_harpoon_items = function()
+  local harpoon = require "harpoon"
+  local marks = harpoon:list().items
+  local current_file_path = vim.fn.expand "%:p:."
+  local label = {}
+
+  for id, item in ipairs(marks) do
+    if item.value == current_file_path then
+      table.insert(label, { id .. " ", active = true })
+    else
+      table.insert(label, { id .. " " })
+    end
+  end
+
+  return label
+end
+
 M.load = function(status)
   local mode = vim.api.nvim_get_mode()["mode"]
   local color = mode_colors[mode] or "#9ccfd8"
   local mode_icon = mode_icons[mode] or "󰋜 "
-  local f_icon = utilities.get_file_icon(vim.fn.expand "%:t", vim.fn.expand "%:e")
-  local edited = vim.bo.mod and " 󰏫 " or ""
+  local harpoon_items = get_harpoon_items()
+
   if status == "active" then
     utilities.colorize("StatusLine", color, color)
   else
     utilities.colorize("StatusLine", "#908caa", "#191724")
   end
   utilities.colorize("StatusLineNC", "#908caa", "#191724")
+  utilities.colorize("StsLnActiveItem", "#e0def4", color)
+
   local stsln = ""
   stsln = " " .. mode_icon .. " "
-  stsln = stsln .. f_icon .. " %t " .. edited .. " "
+  if #harpoon_items > 0 then
+    stsln = stsln .. "%=󰛢 "
+    for _, item in ipairs(harpoon_items) do
+      stsln = stsln .. item[1]
+    end
+  end
   stsln = stsln .. "%= %l:%c 󰧱 "
   return stsln
 end
